61665546047f1b2940197ebe69758eb24ec2be42
tienshake <tientrancntt@gmail.com>
2024-07-29 02:24:09 +0700

    feat: fqa page

---
+++ src/app/pages/home/components/fqa/fqa.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ParallaxDirective } from '../../../../directives/parallax.directive';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { AboutComponent } from "../about/about.component";
+
+@Component({
+  selector: 'app-fqa',
+  templateUrl: './fqa.component.html',
+  styleUrls: ['./fqa.component.scss'],
+  standalone: true,
+  imports: [ParallaxDirective, ButtonComponent, AboutComponent],
+})
+export class FqaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { ParallaxDirective } from '../../directives/parallax.directive';
+import { AboutComponent } from './components/about/about.component';
+import { FqaComponent } from './components/fqa/fqa.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [ButtonComponent, ParallaxDirective],
+  imports: [ButtonComponent, ParallaxDirective, AboutComponent, FqaComponent],
 })
 export class HomeComponent implements OnInit {
   ngOnInit(): void {}
