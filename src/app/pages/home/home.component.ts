a71686eb3b4ce022d116afb9d0985c603ef424ed
dongpham2 <dongpham21082001@gmail.com>
2024-07-28 22:32:37 +0700

    Feat: How to works

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { ParallaxDirective } from '../../directives/parallax.directive';
 import { CardComponent } from '../../components/card/card.component';
+import { SoundstepComponent } from '../../soundstep/soundstep.component';
+import { WorkstepComponent } from '../../workstep/workstep.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [ButtonComponent, ParallaxDirective, CardComponent],
+  imports: [ButtonComponent, ParallaxDirective, CardComponent, SoundstepComponent, WorkstepComponent],
 })
 export class HomeComponent implements OnInit {
   ngOnInit(): void {}
