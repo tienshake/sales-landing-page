95b8d8b35b12936470927e81a4d66c2efb595893
tienshake <tientrancntt@gmail.com>
2024-07-31 01:27:03 +0700

    fix: scroll ending

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Sales
+# sales-landing-page
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.1.2.
 
--- src/app/components/ending/ending.component.ts
+++ src/app/components/ending/ending.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { EndingstepComponent } from '../endingstep/endingstep.component';
 import { ParallaxDirective } from '../../directives/parallax.directive';
+import { ParallaxCenterDirective } from '../../directives/parallax-center.directive';
 
 @Component({
   selector: 'app-ending',
   standalone: true,
-  imports: [ButtonComponent, EndingstepComponent, ParallaxDirective],
+  imports: [
+    ButtonComponent,
+    EndingstepComponent,
+    ParallaxDirective,
+    ParallaxCenterDirective,
+  ],
   templateUrl: './ending.component.html',
   styleUrl: './ending.component.scss',
 })
